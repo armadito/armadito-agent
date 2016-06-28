b7b330aa8a8efbeda4c856390f24a0d2f42c7252
Valentin Hamon <vhamon@teclib.com>
2016-06-28 19:59:06 +0200

    Add default configuration loading

--- lib/Armadito/Agent/Config.pm
+++ lib/Armadito/Agent/Config.pm
@@ -3,13 +3,42 @@ package FusionInventory::Agent::Config;
 use strict;
 use warnings;
 
+my $default = {
+    'ca-cert-dir'             => undef,
+    'ca-cert-file'            => undef,
+    'color'                   => undef,
+    'conf-reload-interval'    => 0,
+    'debug'                   => undef,
+    'force'                   => undef,
+    'html'                    => undef,
+    'local'                   => undef,
+    'logger'                  => 'Stderr',
+    'logfile'                 => undef,
+    'logfacility'             => 'LOG_USER',
+    'logfile-maxsize'         => undef,
+    'no-ssl-check'            => undef,
+    'proxy'                   => undef,
+    'server'                  => undef,
+    'stdout'                  => undef
+};
+
 sub new {
     my ($class, %params) = @_;
 
     my $self = {};
     bless $self, $class;
 
+	$self->_loadDefaults();
+
     return $self;
+}
+
+sub _loadDefaults {
+    my ($self) = @_;
+
+    foreach my $key (keys %$default) {
+        $self->{$key} = $default->{$key};
+    }
 }
 
 1;
