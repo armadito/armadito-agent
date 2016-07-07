6489ba03220aff3b2db124319414d7992a0ab6c2
Valentin Hamon <vhamon@teclib.com>
2016-07-07 11:20:29 +0200

    Add new Getjobs tasks

---
+++ lib/Armadito/Agent/Task/Getjobs.pm
@@ -0,0 +1,114 @@
+package Armadito::Agent::Task::Getjobs;
+
+use strict;
+use warnings;
+use base 'Armadito::Agent::Task';
+
+use FusionInventory::Agent::Config;
+use FusionInventory::Agent::Logger;
+use Data::Dumper;
+use JSON;
+
+sub isEnabled {
+    my ($self) = @_;
+
+    return 1;
+}
+
+sub new {
+    my ($class, %params) = @_;
+
+    my $self = $class->SUPER::new(%params);
+
+    if ($params{debug}) {
+        $self->{debug} = 1;
+    }
+
+    return $self;
+}
+
+sub _handleResponse {
+
+    my ($self, $response) = @_;
+
+    # Parse response
+    # print Dumper($response);
+    print "Successful Response : ".$response->content()."\n";
+
+    my $obj =  from_json($response->content(), { utf8  => 1 });
+
+    print Dumper($obj);
+
+    return $self;
+}
+
+sub _handleError {
+
+    my ($self, $response) = @_;
+
+    # Parse response
+    # print Dumper($response);
+    print "Error Response : ".$response->content()."\n";
+
+    my $obj =  from_json($response->content(), { utf8  => 1 });
+
+    print Dumper($obj);
+
+    return $self;
+}
+
+
+sub run {
+    my ( $self, %params ) = @_;
+
+    $self = $self->SUPER::run(%params);
+
+    my $response = $self->{client}->send(
+        "url" => $self->{config}->{plugin_server_url},
+        args  => { 
+            action    => "pullrequest",
+            agentid => $self->{agentid}
+        },
+		method => "GET"
+    );
+
+    if($response->is_success()){
+         $self->_handleResponse($response);
+         $self->{logger}->info("Getjobs successful...");
+    }
+    else{
+         $self->_handleError($response);
+         $self->{logger}->info("Getjobs failed...");
+    }
+    
+    return $self;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Armadito::Agent::Task::Getjobs - Getjobs Task base class.
+
+=head1 DESCRIPTION
+
+This task inherits from L<Armadito::Agent::Task>. Send a pull GET request to get jobs agent has to do according to Armadito Plugin for GLPI.
+
+=head1 FUNCTIONS
+
+=head2 isEnabled ( $self )
+
+Returns true if the task is enabled.
+
+=head2 run ( $self, %params )
+
+Run the task.
+
+=head2 new ( $self, %params )
+
+Instanciate Task.
+
+
+
