047fa27c71158d9b4187085ca9f9dd26fe88245f
Valentin Hamon <vhamon@teclib.com>
2016-07-28 12:26:37 +0200

    get Fusion Installlocation path to use the mini strawberry as the perl interpreter for Armadito-agent

--- bin/armadito-agent.bat
+++ bin/armadito-agent.bat
@@ -1,2 +1,18 @@
 @echo off
-"C:\\Program Files\\FusionInventory-Agent\\perl\\bin\\perl.exe" armadito-agent
+
+for /f "tokens=2*" %%a in ('REG QUERY "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\FusionInventory-Agent" /v InstallLocation') do set "FusionPath=%%~b"
+
+if defined FusionPath (
+	if not exist "%FusionPath%\\perl\\bin\\perl.exe" (
+		echo FusionInventory Agent embedded perl not found.
+		exit /b
+	) else (
+		"%FusionPath%\\perl\\bin\\perl.exe" armadito-agent
+		exit /b
+	) 
+) else (
+    echo FusionInventory Agent installLocation not found.
+	exit /b
+)
+
+REM "C:\\Program Files\\FusionInventory-Agent\\perl\\bin\\perl.exe" armadito-agent
