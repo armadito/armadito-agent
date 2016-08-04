ee4438e2245e412255dc53f89a4ca22a17b075af
Valentin Hamon <vhamon@teclib.com>
2016-08-04 17:43:16 +0200

    Remove unused code in Scan::Armadito class

--- lib/Armadito/Agent/Task/Scan/Armadito.pm
+++ lib/Armadito/Agent/Task/Scan/Armadito.pm
@@ -52,45 +52,7 @@ sub run {
     my ( $self, %params ) = @_;
 
     $self = $self->SUPER::run(%params);
-
-    # TODO:
-    # 1 : Send GET request to AV, asking for an on-demand scan
-    # my $req = $self->{client}->send(
-    #    "url" => $self->{config}->{av_server_url},
-    #    method => "GET"
-    #    args  => {
-    #        action    => "getState"
-    #    }
-    #);
-
-    my $av_response = '
-{ "av_response":"state",
-  "id":123,
-  "status":0,
-  "scan": "OK"
-}';
-	my $state_jobj =  from_json($av_response, { utf8  => 1 });
-
-	$self->{jobj}->{task}->{msg} = $state_jobj;
-
-	my $json_text = to_json($self->{jobj});
-
-	print "JSON formatted str : \n".$json_text."\n";
-
-    my $response = $self->{client}->send(
-		"url" => $self->{config}->{plugin_server_url}."/api/scans",
-		message => $json_text,
-		method => "POST"
-    );
-
-    if($response->is_success()){
-         $self->_handleResponse($response);
-         $self->{logger}->info("Scan successful...");
-    }
-    else{
-         $self->_handleError($response);
-         $self->{logger}->info("Scan failed...");
-    }
+	$self->{logger}->info("Armadito Scan launched.");
 
     return $self;
 }
