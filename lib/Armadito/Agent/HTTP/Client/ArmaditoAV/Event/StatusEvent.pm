542a149261dd67931995dbd3b31891e4bb14d345
Valentin Hamon <vhamon@teclib.com>
2016-08-24 16:37:26 +0200

    Fix #22 - Implements Send ArmaditoAV status to GLPI plugin

--- lib/Armadito/Agent/HTTP/Client/ArmaditoAV/Event/StatusEvent.pm
+++ lib/Armadito/Agent/HTTP/Client/ArmaditoAV/Event/StatusEvent.pm
@@ -3,6 +3,7 @@ package Armadito::Agent::HTTP::Client::ArmaditoAV::Event::StatusEvent;
 use strict;
 use warnings;
 use base 'Armadito::Agent::HTTP::Client::ArmaditoAV::Event';
+use JSON;
 
 sub new {
 	my ( $class, %params ) = @_;
@@ -12,8 +13,36 @@ sub new {
 	return $self;
 }
 
+sub _sendToGLPI {
+	my ( $self, $message ) = @_;
+
+	my $response = $self->{taskobj}->{glpi_client}->sendRequest(
+		"url"   => $self->{taskobj}->{agent}->{config}->{armadito}->{server} . "/api/states",
+		message => $message,
+		method  => "POST"
+	);
+
+	if ( $response->is_success() ) {
+		$self->{taskobj}->_handleResponse($response);
+		$self->{taskobj}->{logger}->info("Send ArmaditoAV State successful...");
+	}
+	else {
+		$self->{taskobj}->_handleError($response);
+		$self->{taskobj}->{logger}->info("Send ArmaditoAV State failed...");
+	}
+
+	return;
+}
+
 sub run {
 	my ( $self, %params ) = @_;
+
+	$self->{taskobj}->{jobj}->{task}->{obj} = $self->{jobj};
+	my $json_text = to_json( $self->{taskobj}->{jobj} );
+	print "JSON formatted str : \n" . $json_text . "\n";
+
+	$self->_sendToGLPI($json_text);
+	$self->{end_polling} = 1;
 
 	return $self;
 }
@@ -33,7 +62,7 @@ This is the class dedicated to StatusEvent of ArmaditoAV api.
 
 =head2 run ( $self, %params )
 
-Run event related stuff.
+Run event related stuff. Send ArmaditoAV status to Armadito Plugin for GLPI.
 
 =head2 new ( $class, %params )
 
--- lib/Armadito/Agent/Task/State/Armadito.pm
+++ lib/Armadito/Agent/Task/State/Armadito.pm
@@ -52,81 +52,19 @@ sub run {
 	my ( $self, %params ) = @_;
 
 	$self = $self->SUPER::run(%params);
+	$self->{av_client} = Armadito::Agent::HTTP::Client::ArmaditoAV->new( taskobj => $self );
+	$self->{av_client}->register();
 
-	# 1 : Send GET request to AV, asking for AV state
-	# 2 : Handle AV response
-	# 3 : If successful, encapsulate AV state in a POST request to Armadito Plugin for GLPI
-
-	# TODO:
-	# 1 : Send GET request to AV, asking for AV state
-	# my $req = $self->{armaditoAV_client}->sendRequest(
-	#    "url" => $self->{config}->{av_server_url},
-	#    method => "GET"
-	#    args  => {
-	#        action    => "getState"
-	#    }
-	#);
-
-	my $av_response = '
-{ "av_response":"state", 
-  "id":123, 
-  "status":0, 
-  "info" : {
-    "antivirus" : {
-      "version": "3.14",
-      "realtime": "on",
-      "service": "on"
-    },
-    "agent": {
-      "status": "on",
-      "version": "1.0"
-    },
-    "update": {
-      "status": "up-to-date",
-      "last-update": "1970-01-01 00:00"
-    },
-    "modules": [
-      {
-        "name": "clamav",
-        "version": "1.2",
-        "update": {
-          "status": "up-to-date",
-          "last-update": "1970-01-01 00:00"
-        }
-      },
-      {
-        "name": "moduleH1",
-        "version": "1.0",
-        "update": {
-          "status": "up-to-date",
-          "last-update": "1970-01-01 00:00"
-        }
-      }
-    ]
-  }
-}';
-	my $state_jobj = from_json( $av_response, { utf8 => 1 } );
-
-	$self->{jobj}->{task}->{obj} = $state_jobj;
-
-	my $json_text = to_json( $self->{jobj} );
-
-	print "JSON formatted str : \n" . $json_text . "\n";
-
-	my $response = $self->{glpi_client}->sendRequest(
-		"url"   => $self->{agent}->{config}->{armadito}->{server} . "/api/states",
-		message => $json_text,
-		method  => "POST"
+	my $response = $self->{av_client}->sendRequest(
+		"url"  => $self->{av_client}->{server_url} . "/api/status",
+		method => "GET"
 	);
 
-	if ( $response->is_success() ) {
-		$self->_handleResponse($response);
-		$self->{logger}->info("State successful...");
-	}
-	else {
-		$self->_handleError($response);
-		$self->{logger}->info("State failed...");
-	}
+	die "Unable to get AV status with ArmaditoAV api."
+		if ( !$response->is_success() );
+
+	$self->{av_client}->pollEvents();
+	$self->{av_client}->unregister();
 
 	return $self;
 }
