b4e36ba845ebaac266e242e2681033c06831063c
Valentin Hamon <vhamon@teclib.com>
2016-11-29 16:16:49 +0100

    add getOS info in antivirus base class

--- lib/Armadito/Agent/Antivirus.pm
+++ lib/Armadito/Agent/Antivirus.pm
@@ -2,15 +2,38 @@ package Armadito::Agent::Antivirus;
 
 use strict;
 use warnings;
+use English qw(-no_match_vars);
 
 sub new {
 	my ( $class, %params ) = @_;
 
-	my $self = { logger => $params{logger} };
+	my $self = {
+		logger => $params{logger},
+		os_info => ""
+	};
 
 	bless $self, $class;
+
+	$self->{os_info} = $self->getOperatingSystemInfo();
+
 	return $self;
 }
+
+sub getOperatingSystemInfo {
+	my ($self) = @_;
+
+	my $os_info = {
+		name => $OSNAME,
+		libpath => $OSNAME
+	};
+
+	if($OSNAME eq "MSWin32"){
+		$os_info->{libpath} = "Win32";
+	}
+
+	return $os_info;
+}
+
 1;
 
 __END__
@@ -28,4 +51,8 @@ This is a base class for all stuff specific to an Antivirus.
 =head2 new ( $self, %params )
 
 Instanciate Armadito module.
+
+=head2 getOperatingSystemInfo ( $self )
+
+Get Operating system information.
 
