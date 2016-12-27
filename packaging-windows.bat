ab7e45c26e931ef62abe0f022d5dbcf5e2b5fc84
Valentin Hamon <vhamon@teclib.com>
2016-12-27 11:18:07 +0100

    rm useless exit at end of packaging windows script

--- packaging-windows.bat
+++ packaging-windows.bat
@@ -6,4 +6,3 @@ set programpath=%cd%
 call pl2bat %programpath%\bin\armadito-agent
 
 "C:\Program Files (x86)\Inno Setup 5\iscc" /Qp /dMyAppVersion=%version% %programpath%\Armadito-Agent.iss
-exit /b %errorlevel%
