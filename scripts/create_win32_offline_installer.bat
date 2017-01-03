8582c2f232e415f687a2147775e84540c60a0c4a
Valentin Hamon <vhamon@teclib.com>
2017-01-03 15:32:33 +0100

    copy and prepare offline win32installer from script

--- scripts/create_win32_offline_installer.bat
+++ scripts/create_win32_offline_installer.bat
@@ -9,6 +9,7 @@ set strawberry_version=5.24.0.1
 
 set download_url=http://strawberryperl.com/download/%strawberry_version%/strawberry-perl-%strawberry_version%-32bit.msi
 if not exist %programpath%\res\strawberry-perl-%strawberry_version%-32bit.msi wget %download_url% -P %programpath%\res
+copy "%programpath%\res\strawberry-perl-%strawberry_version%-32bit.msi" "%programpath%\res\strawberry-perl.msi"
 
-REM -- call pl2bat %programpath%\bin\armadito-agent
-REM -- "C:\Program Files (x86)\Inno Setup 5\iscc" /Qp /dMyAppVersion=%version% %programpath%\res\Armadito-Agent-Offline.iss
+call pl2bat %programpath%\bin\armadito-agent
+"C:\Program Files (x86)\Inno Setup 5\iscc" /Qp /dMyAppVersion=%version% %programpath%\res\Armadito-Agent-Offline.iss
