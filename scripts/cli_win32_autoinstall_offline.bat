9ffa85de5209ae0d0d2060609fd51b3f9e13cc28
Valentin Hamon <vhamon@teclib.com>
2017-01-03 17:37:39 +0100

    Add cli autoinstall script for offline win32 installer

---
+++ scripts/cli_win32_autoinstall_offline.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+
+set version=0.1.0_02
+set programpath=%~dp0\..
+
+%programpath%\out\Armadito-Agent-%version%-Setup-Offline.exe ^
+    /SP- /VERYSILENT /LOG=%programpath%\out\setuplog.txt
+
+REM -- To change default install directory : /DIR=C:\Armadito-Agent
+REM -- For further informations, see : http://www.jrsoftware.org/ishelp/index.php?topic=setupcmdline
