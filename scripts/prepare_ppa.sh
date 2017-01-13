069fc9756920524b3f89dd834db119627242043b
Valentin Hamon <vhamon@teclib.com>
2017-01-13 17:10:15 +0100

    Improve prepare ppa script

--- scripts/prepare_ppa.sh
+++ scripts/prepare_ppa.sh
@@ -5,7 +5,7 @@ OPT=$1
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOTDIR=$DIR/../
 TARBALL_VERSION=$(cat $ROOTDIR/version)
-BUILD_VERSION=1
+BUILD_VERSION=$1
 
 cd $ROOTDIR
 
@@ -16,4 +16,4 @@ set -e
 $ROOTDIR/scripts/deb-src.sh -d xenial -k EB623C6B -v $BUILD_VERSION -D $ROOTDIR/out/ $ROOTDIR/libarmadito-agent-perl-$TARBALL_VERSION.tar.gz
 
 # ===================
-dput -u ppa:armadito/armadito-agent $BUILD_DIR/${PKG}_${DEBIAN_VERSION}${BUILD_VERSION}~${DISTRIB}_source.changes
+# dput -u ppa:armadito/armadito-agent $BUILD_DIR/${PKG}_${DEBIAN_VERSION}${BUILD_VERSION}~${DISTRIB}_source.changes
